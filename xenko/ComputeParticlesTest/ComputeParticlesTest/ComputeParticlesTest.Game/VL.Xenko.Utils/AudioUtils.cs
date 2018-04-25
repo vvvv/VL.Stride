2f6fa3e85f4c65de894faabed365ec369ebd3714
tebjan <tebjan@vvvv.org>
2018-04-26 01:46:33 +0200

    re-added util classes
    Former-commit-id: b959f6e42b76972ef65c3187b433ee92e88a2ee3

--- xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/VL.Xenko.Utils/AudioUtils.cs
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/VL.Xenko.Utils/AudioUtils.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using SiliconStudio.Xenko.Audio;
+using SiliconStudio.Xenko.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Xenko.Utils
+{
+    public static class AudioUtils
+    {
+        public static AudioEmitterSoundController GetSoundController(Entity entity, string key)
+        {
+            return entity.Get<AudioEmitterComponent>()[key];
+        }
+
+        public static Entity RemoveAudioEmitter(Entity entity)
+        {
+            var emitter = entity.Get<AudioEmitterComponent>();
+            entity.Components.Remove<AudioEmitterComponent>();
+            return entity;
+        }
+    }
+}
