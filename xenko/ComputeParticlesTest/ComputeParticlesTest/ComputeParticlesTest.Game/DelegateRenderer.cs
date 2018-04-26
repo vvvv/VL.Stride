a07892f0e7fd7fa16257210e3e450deffdbd2add
tebjan <tebjan@vvvv.org>
2018-04-26 04:10:49 +0200

    added noise textures, InterfaceRenderer and works with sprite 3d batch
    Former-commit-id: cff88cd467d02b7b7e9fa720c8698a2f966f8d31

---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/DelegateRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
+using SiliconStudio.Xenko.Rendering;
+using SiliconStudio.Xenko.Rendering.Compositing;
+using SiliconStudio.Xenko.Graphics;
+
+namespace ComputeParticlesTest
+{
+    public class DelegateRenderer : SceneRendererBase
+    {
+        public Action InitializeAction { get; set; }
+        public Action<RenderContext> CollectAction { get; set; }
+        public Action<RenderContext, RenderDrawContext, CommandList> DrawAction { get; set; }
+
+        protected override void CollectCore(RenderContext context)
+        {
+            base.CollectCore(context);
+            CollectAction?.Invoke(context);
+        }
+
+        protected override void InitializeCore()
+        {
+            base.InitializeCore();
+            InitializeAction?.Invoke();
+        }
+
+        protected override void DrawCore(RenderContext context, RenderDrawContext drawContext)
+        {
+            DrawAction?.Invoke(context, drawContext, drawContext.CommandList);
+        }
+    }
+}
---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/InterfaceRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
+using SiliconStudio.Xenko.Rendering;
+using SiliconStudio.Xenko.Rendering.Compositing;
+using SiliconStudio.Xenko.Graphics;
+
+namespace ComputeParticlesTest
+{
+    public interface ICanRender
+    {
+        void Initialize();
+
+        void Collect(RenderContext context);
+
+        void Draw(RenderContext renderContext, RenderDrawContext drawContext, CommandList commandList);
+    }
+
+    public class InterfaceRenderer : SceneRendererBase
+    {
+        public ICanRender Renderer { get; set; }
+
+        protected override void InitializeCore()
+        {
+            base.InitializeCore();
+            Renderer?.Initialize();
+        }
+
+        protected override void CollectCore(RenderContext context)
+        {
+            base.CollectCore(context);
+            Renderer?.Collect(context);
+        }
+
+        protected override void DrawCore(RenderContext context, RenderDrawContext drawContext)
+        {
+            Renderer?.Draw(context, drawContext, drawContext.CommandList);
+        }
+    }
+}
