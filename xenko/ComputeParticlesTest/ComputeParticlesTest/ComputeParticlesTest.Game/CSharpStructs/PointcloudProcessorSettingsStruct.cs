952685eec5fa86ae2311cd09ecb116841305eb5f
N-LAPTOP\natan <natan.sinigaglia@gmail.com>
2018-04-28 18:49:16 +0200

    added pointcloud settings structure.cs
    Former-commit-id: 77359e5c916cab51eb2b75238bea92747060e5b0 [formerly 46bcfbbb73c44a5f5f2d7aca56c27ad23ed50b27]
    Former-commit-id: 1f1a747df41cd9b3858ef9febf7b8f40432d258e

---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/CSharpStructs/PointcloudProcessorSettingsStruct.cs
@@ -0,0 +1,29 @@
+﻿using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeParticlesTest
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PointcloudProcessorSettings
+    {
+        float Points_Size;
+        float Culling_DotThreshold;
+        Matrix Pointcloud_T;
+        float pad1;
+        float pad2;
+
+        public PointcloudProcessorSettings(float points_Size, float culling_DotThreshold, Matrix pointcloud_T, float pad1, float pad2)
+        {
+            Points_Size = points_Size;
+            Culling_DotThreshold = culling_DotThreshold;
+            Pointcloud_T = pointcloud_T;
+            this.pad1 = pad1;
+            this.pad2 = pad2;
+        }
+    }
+}
