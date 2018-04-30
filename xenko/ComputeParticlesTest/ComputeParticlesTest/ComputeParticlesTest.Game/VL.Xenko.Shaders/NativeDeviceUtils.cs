f6ea6752e5632831496ca109acc600581d784508
tebjan <tebjan@vvvv.org>
2018-04-30 04:55:48 +0200

    added ResetCounter Hack with working culling, started indirect draw Hack
    Former-commit-id: 13ab1e871e2d1e326b2077cc35c6cf9c8ea12933 [formerly acfc566db4b3049f3913d2b40c810548ec37d032]
    Former-commit-id: f6dbba54673bcc82387e08d22f816870554b1dbc

---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/VL.Xenko.Shaders/NativeDeviceUtils.cs
@@ -0,0 +1,42 @@
+﻿using SiliconStudio.Xenko.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Xenko.Shaders
+{
+    public static class NativeDeviceUtils
+    {
+        static FieldInfo nativeDeviceContextFi;
+
+        static FieldInfo NativeDeviceContextFi
+        {
+            get
+            {
+                if(nativeDeviceContextFi == null)
+                {
+                    var type = VLHDE.GameInstance.GraphicsContext.CommandList.GetType();
+                    nativeDeviceContextFi = type.GetRuntimeFields().Where(fi => fi.Name == "nativeDeviceContext").First();
+                }
+                return nativeDeviceContextFi;
+            }
+        }
+
+        public static SharpDX.Direct3D11.DeviceContext GetNativeDeviceContext(this CommandList commandList)
+            => (SharpDX.Direct3D11.DeviceContext)NativeDeviceContextFi.GetValue(commandList);
+
+        public static void DrawInstancedIndirect(CommandList commandList)
+        {
+            var nativeDeviceContext = GetNativeDeviceContext(commandList);
+           // nativeDeviceContext.DrawInstancedIndirect();
+        }
+
+        internal static void ComputeShaderSetUnorderedAccessView(this CommandList commandList, int slot, SharpDX.Direct3D11.UnorderedAccessView unorderedAccessView, int counterValue)
+        {
+            commandList.GetNativeDeviceContext().ComputeShader.SetUnorderedAccessView(slot, unorderedAccessView, counterValue);
+        }
+    }
+}
