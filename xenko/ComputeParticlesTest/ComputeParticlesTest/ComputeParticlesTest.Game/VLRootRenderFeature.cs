3c8196ab6743dc258739bb4da6951a42ac51f55d
tebjan <tebjan@vvvv.org>
2018-05-03 15:06:51 +0200

    moved materials into their own folder, removed ClearCoat and can now switch material by left/right key
    Former-commit-id: 8d46d014894777ffd586d95e2c50da6e069b1bc5 [formerly 167a956546bf071ea3e188760dcc073c5e06a436]
    Former-commit-id: af8fe0a5ae8f7529564d43f769b6390b572821c7

---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/VLRootRenderFeature.cs
@@ -0,0 +1,77 @@
+﻿using SiliconStudio.Xenko.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Xenko.Graphics;
+
+namespace ComputeParticlesTest
+{
+    public partial class VLRootRenderFeature : RootEffectRenderFeature
+    {
+        public override Type SupportedRenderObjectType => typeof(object);
+
+        public ICanRender Renderer { get; set; }
+
+        public VLRootRenderFeature()
+            : base()
+        {
+        }
+
+        protected override void InitializeCore()
+        {
+            base.InitializeCore();
+        }
+
+        public override void Collect()
+        {
+            base.Collect();
+        }
+
+        public override void Extract()
+        {
+            base.Extract();
+        }
+
+        public override void Prepare(RenderDrawContext context)
+        {
+            base.Prepare(context);
+        }
+
+        public override void Draw(RenderDrawContext context, RenderView renderView, RenderViewStage renderViewStage)
+        {
+            base.Draw(context, renderView, renderViewStage);
+        }
+
+        public override void PrepareEffectPermutations(RenderDrawContext context)
+        {
+            base.PrepareEffectPermutations(context);
+        }
+
+        public override void PrepareEffectPermutationsImpl(RenderDrawContext context)
+        {
+            base.PrepareEffectPermutationsImpl(context);
+        }
+
+        protected override void ProcessPipelineState(RenderContext context, RenderNodeReference renderNodeReference, ref RenderNode renderNode, RenderObject renderObject, PipelineStateDescription pipelineState)
+        {
+            base.ProcessPipelineState(context, renderNodeReference, ref renderNode, renderObject, pipelineState);
+        }
+
+        protected override void InvalidateEffectPermutation(RenderObject renderObject, RenderEffect renderEffect)
+        {
+            base.InvalidateEffectPermutation(renderObject, renderEffect);
+        }
+
+        protected override void OnAddRenderObject(RenderObject renderObject)
+        {
+            base.OnAddRenderObject(renderObject);
+        }
+
+        protected override int ComputeDataArrayExpectedSize(DataType type)
+        {
+            return base.ComputeDataArrayExpectedSize(type);
+        }
+    }
+}
