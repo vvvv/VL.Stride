2ccd3e6da1a4abff6bcbbcfc5fb52eed772bf3f6
tebjan <tebjan@vvvv.org>
2018-08-07 16:24:50 +0200

    update standalone host
    Former-commit-id: 9dfd0ac8631dabab5728ce56599ee14a6560ec34 [formerly f57ac6dbef3b33dcaa4c08b3f0baeb08adbd30f4]
    Former-commit-id: 5198168c299f9826ee65b5a21621c25aea80f2fe

---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/VL/Update_VL.bat
@@ -0,0 +1,27 @@
+set source="C:\Dev\vvvv\vvvv\public\build\vvvv_50debug36.1_x64\lib\packs\"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\VL\packs\"
+xcopy %source% %destination% /y /s
+
+set source="C:\Dev\vvvv\vvvv\vvvv50\VL.StandaloneHost\src\bin\AnyCPU\Release\VL.StandaloneHost.dll"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\ComputeParticlesTest.Windows\Dependencies\VL.StandaloneHost.dll"
+xcopy %source% %destination% /y 
+
+set source="C:\Dev\vvvv\vvvv\vvvv50\VL.StandaloneHost\src\bin\AnyCPU\Release\VL.StandaloneHost.pdb"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\ComputeParticlesTest.Windows\Dependencies\VL.StandaloneHost.pdb"
+xcopy %source% %destination% /y 
+
+set source="C:\Dev\vvvv\vvvv\vvvv50\VL.StandaloneHost\src\bin\AnyCPU\Release\VL.Lang.dll"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\ComputeParticlesTest.Windows\Dependencies\VL.Lang.dll"
+xcopy %source% %destination% /y 
+
+set source="C:\Dev\vvvv\vvvv\vvvv50\VL.StandaloneHost\src\bin\AnyCPU\Release\VL.Lang.pdb"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\ComputeParticlesTest.Windows\Dependencies\VL.Lang.pdb"
+xcopy %source% %destination% /y 
+
+set source="C:\Dev\vvvv\vvvv\vvvv50\VL.StandaloneHost\src\bin\AnyCPU\Release\VL.NuGetAssemblyLoader.dll"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\ComputeParticlesTest.Windows\Dependencies\VL.NuGetAssemblyLoader.dll"
+xcopy %source% %destination% /y 
+
+set source="C:\Dev\vvvv\vvvv\vvvv50\VL.StandaloneHost\src\bin\AnyCPU\Release\VL.NuGetAssemblyLoader.pdb"
+set destination="C:\vvvv\PROJECTS\2018\05_VL_Xenko\VL.Xenko\xenko\ComputeParticlesTest\ComputeParticlesTest\ComputeParticlesTest.Windows\Dependencies\VL.NuGetAssemblyLoader.pdb"
+xcopy %source% %destination% /y 
