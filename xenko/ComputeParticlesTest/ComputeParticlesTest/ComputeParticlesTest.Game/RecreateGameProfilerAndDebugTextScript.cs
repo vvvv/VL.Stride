261025ec1262c9c8f3db6d3d5fcd08b13a3d8b6b
tebjan <tebjan@vvvv.org>
2018-08-07 15:13:43 +0200

    added fix for profiler script
    Former-commit-id: b915de6fb434e95b42bf196d94dca0940f5805ad [formerly c41b2dff380af53735edf50442c3345914299c62]
    Former-commit-id: d55f2ab405bf2c06f87461f55d86debc1bf8bbcf

---
+++ xenko/ComputeParticlesTest/ComputeParticlesTest/ComputeParticlesTest.Game/RecreateGameProfilerAndDebugTextScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Xenko.Engine;
+using Xenko.Profiling;
+
+namespace ComputeParticlesTest
+{
+    public class RecreateGameProfilerAndDebugTextScript : StartupScript
+    {
+        public override void Start()
+        {
+            // GameProfiler
+            GameProfiler.DisableProfiling();
+            Game.GameSystems.Remove(GameProfiler);
+            Services.RemoveService<GameProfilingSystem>();
+
+            var gameProfiler = new GameProfilingSystem(Services);
+            Services.AddService(gameProfiler);
+            Game.GameSystems.Add(gameProfiler);
+
+            // DebugText
+            DebugText.Enabled = false;
+            DebugText.Visible = false;
+            Game.GameSystems.Remove(DebugText);
+            Services.RemoveService<DebugTextSystem>();
+
+            var debugText = new DebugTextSystem(Services);
+            Services.AddService(debugText);
+            Game.GameSystems.Add(debugText);
+
+            // Reload current scene
+
+            var result = Content.TryGetAssetUrl(SceneSystem.SceneInstance.RootScene, out var url);
+            if (!result)
+            {
+            }
+
+            Content.Unload(SceneSystem.SceneInstance.RootScene);
+
+            var scene = Content.Load<Scene>(url);
+            var script = scene.Entities.SelectMany((e) => e.Components).OfType<RecreateGameProfilerAndDebugTextScript>().First();
+            script.Entity.Remove(script);
+
+            SceneSystem.SceneInstance.RootScene = scene;
+        }
+    }
+}
