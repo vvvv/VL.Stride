6886fa97f260050efd567072859d19f90405203b
tebjan <tebjan@vvvv.org>
2018-08-15 18:08:08 +0200

    moved util things from demo project to VL.Xenko.csproj
    Former-commit-id: 4af8de278f6257e382ad9a1ffd85f3dd1e58c8aa [formerly eecb87d7fb7e691a18c6cffd95a5b653a6e237d7]
    Former-commit-id: 8f82e97ee1c98d0cbb501943c3769ffa4802ef63

---
+++ src/VL.Xenko.Utils/CameraUtils.cs
@@ -0,0 +1,46 @@
+using Xenko.Core.Mathematics;
+using Xenko.Engine;
+using Xenko.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Xenko.Utils
+{
+    public static class CameraUtils
+    {
+        public static CameraComponent GetFirstCamera()
+        {
+            return VLScript.GameInstance.SceneSystem.GraphicsCompositor.Cameras[0].Camera;
+        }
+
+        public static IMouseDevice GetFirstMouse()
+        {
+            return VLScript.GameInstance.Input.Mouse;
+        }
+
+        public static Ray ScreenToWorldRay(CameraComponent cameraComponent, Vector2 position)
+        {
+            if (cameraComponent == null)
+            {
+                throw new ArgumentNullException(nameof(cameraComponent));
+            }
+
+            Matrix inverseViewProjection = Matrix.Invert(cameraComponent.ViewProjectionMatrix);
+
+            Vector3 clipSpace;
+            clipSpace.X = position.X * 2f - 1f;
+            clipSpace.Y = 1f - position.Y * 2f;
+
+            clipSpace.Z = 0f;
+            var near = Vector3.TransformCoordinate(clipSpace, inverseViewProjection);
+
+            clipSpace.Z = 1f;
+            var far = Vector3.TransformCoordinate(clipSpace, inverseViewProjection);
+
+            return new Ray(near, Vector3.Normalize(far - near));
+        }
+    }
+}
---
+++ src/VL.Xenko.Utils/ModelUtils.cs
@@ -0,0 +1,47 @@
+using Xenko.Core.Mathematics;
+using Xenko.Engine;
+using Xenko.Rendering;
+using Xenko.Rendering.Materials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Xenko.Utils
+{
+    public static class ModelUtils
+    {
+        public static Material LoadMaterial(string path)
+        {
+            return VLScript.GameInstance.Content.Load<Material>(path);
+        }
+
+        public static ModelComponent GetOrCreateModelComponent(Entity entity)
+        {
+            return entity.GetOrCreate<ModelComponent>();
+        }
+
+        public static void SetColor(Model model, Color4 color, float metalness)
+        {
+            var m = model.Materials.FirstOrDefault()?.Material.Passes.FirstOrDefault();
+
+            if (m != null)
+            {
+                m.Parameters.Set(MaterialKeys.DiffuseValue, color);
+                m.Parameters.Set(MaterialKeys.MetalnessValue, metalness);
+            }
+
+        }
+
+        public static Entity GetPrefabInstance(string prefabName)
+        {
+            // Note that "MyBulletPrefab" refers to the name and location of your prefab asset
+            var myBulletPrefab = VLScript.GameInstance.Content.Load<Prefab>(prefabName);
+
+            // Instantiate a prefab
+            var instance = myBulletPrefab.Instantiate();
+            return instance[0];
+        }
+    }
+}
