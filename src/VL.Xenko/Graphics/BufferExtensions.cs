71fb4696f5d356d7fcc9b6f14d72006f7ac6a9b5
Elias Holzer <elias@vvvv.org>
2018-11-28 17:28:49 +0100

    DynamicBuffer and SetData nodes will now take new interface IHasMemory implemented by both Spread and SpreadBuilder to avoid memory allocations.

--- src/VL.Xenko/Graphics/BufferExtensions.cs
+++ src/VL.Xenko/Graphics/BufferExtensions.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
-using VL.Lib.Collections;
 using Xenko.Engine;
 using Xenko.Graphics;
 using Xenko.Core.IO;
 using System.Buffers;
 using System.IO;
 using Buffer = Xenko.Graphics.Buffer;
+using VL.Core;
 
 namespace VL.Xenko.Graphics
 {
@@ -26,21 +25,73 @@ namespace VL.Xenko.Graphics
         /// See the unmanaged documentation about Map/UnMap for usage and restrictions.
         /// </remarks>
         /// <returns>The GPU buffer.</returns>
-        public static unsafe Buffer SetData<TData>(this Buffer buffer, CommandList commandList, Spread<TData> fromData, int offsetInBytes = 0) where TData : struct
+        public static unsafe Buffer SetData<TData>(this Buffer buffer, CommandList commandList, IHasMemory<TData> fromData, int offsetInBytes = 0) where TData : struct
         {
-            var immutableArray = fromData._array;
-            var array = Unsafe.As<ImmutableArray<TData>, TData[]>(ref immutableArray);
-            buffer.SetData(commandList, array, offsetInBytes);
+            if (fromData.TryGetMemory(out ReadOnlyMemory<TData> memory))
+                return buffer.SetData(commandList, memory, offsetInBytes);
             return buffer;
         }
 
-        public static unsafe Buffer New<TData>(GraphicsDevice device, Spread<TData> fromData, BufferFlags bufferFlags, GraphicsResourceUsage usage) where TData : struct
+        /// <summary>
+        /// Copies the <paramref name="memory"/> to the given <paramref name="buffer"/> on GPU memory.
+        /// </summary>
+        /// <typeparam name="TData">The type of the T data.</typeparam>
+        /// <param name="buffer">The <see cref="Buffer"/>.</param>
+        /// <param name="commandList">The <see cref="CommandList"/>.</param>
+        /// <param name="memory">The memory to copy from.</param>
+        /// <param name="offsetInBytes">The offset in bytes to write to.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <remarks>
+        /// See the unmanaged documentation about Map/UnMap for usage and restrictions.
+        /// </remarks>
+        /// <returns>The GPU buffer.</returns>
+        public static unsafe Buffer SetData<TData>(this Buffer buffer, CommandList commandList, ReadOnlyMemory<TData> memory, int offsetInBytes = 0) where TData : struct
         {
-            var immutableArray = fromData._array;
-            var array = Unsafe.As<ImmutableArray<TData>, TData[]>(ref immutableArray);
-            return Buffer.New(device, array, bufferFlags, usage);
+            using (var handle = memory.Pin())
+            {
+                var elementSize = Unsafe.SizeOf<TData>();
+                var dataPointer = new DataPointer(handle.Pointer, memory.Length * elementSize);
+                buffer.SetData(commandList, dataPointer, offsetInBytes);
+                return buffer;
+            }
         }
 
+        /// <summary>
+        /// Creates a new <see cref="Buffer"/> initialized with a copy of the given data.
+        /// </summary>
+        /// <typeparam name="TData">The element type.</typeparam>
+        /// <param name="device">The graphics device.</param>
+        /// <param name="fromData">The data to use to initialize the buffer.</param>
+        /// <param name="bufferFlags">The buffer flags.</param>
+        /// <param name="usage">The buffer usage.</param>
+        /// <exception cref="ArgumentException">If retrieval of read-only memory failed.</exception>
+        /// <returns>The newly created buffer.</returns>
+        public static unsafe Buffer New<TData>(GraphicsDevice device, IHasMemory<TData> fromData, BufferFlags bufferFlags, GraphicsResourceUsage usage) where TData : struct
+        {
+            if (fromData.TryGetMemory(out ReadOnlyMemory<TData> memory))
+                return New(device, memory, bufferFlags, usage);
+            throw new ArgumentException($"Failed to create buffer because retrieval of read-only memory failed.", nameof(fromData));
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Buffer"/> initialized with a copy of the given data.
+        /// </summary>
+        /// <typeparam name="TData">The element type.</typeparam>
+        /// <param name="device">The graphics device.</param>
+        /// <param name="memory">The data to use to initialize the buffer.</param>
+        /// <param name="bufferFlags">The buffer flags.</param>
+        /// <param name="usage">The buffer usage.</param>
+        /// <exception cref="ArgumentException">If retrieval of read-only memory failed.</exception>
+        /// <returns>The newly created buffer.</returns>
+        public static unsafe Buffer New<TData>(GraphicsDevice device, ReadOnlyMemory<TData> memory, BufferFlags bufferFlags, GraphicsResourceUsage usage) where TData : struct
+        {
+            using (var handle = memory.Pin())
+            {
+                var elementSize = Unsafe.SizeOf<TData>();
+                var dataPointer = new DataPointer(handle.Pointer, memory.Length * elementSize);
+                return Buffer.New(device, dataPointer, elementSize, bufferFlags, usage);
+            }
+        }
 
 
         //    public static unsafe void WriteToDisk(this Buffer buffer, string filepath)
--- src/VL.Xenko/Interop.cs
+++ src/VL.Xenko/Interop.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 namespace VL.Xenko
 {
@@ -12,13 +11,5 @@ namespace VL.Xenko
         /// <param name="object">The object.</param>
         /// <returns>The size of the object in bytes.</returns>
         public static int SizeOf<T>(T @object) => Unsafe.SizeOf<T>();
-
-        /// <summary>
-        /// Returns the element size of the collection in bytes.
-        /// </summary>
-        /// <typeparam name="T">The element type.</typeparam>
-        /// <param name="collection">The collection.</param>
-        /// <returns>The size of one collection element in bytes.</returns>
-        public static int SizeOfElement<T>(IReadOnlyCollection<T> collection) => Unsafe.SizeOf<T>();
     }
 }
