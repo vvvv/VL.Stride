fbfb1db2cdaa3f113ca27389c3f4110487663b77
Elias Holzer <elias@vvvv.org>
2019-01-29 21:33:42 +0100

    Added VL.Xenko.Integration project, all projects use Nuget.Build.Packaging, VL.Xenko package looking good, VL.Xenko.Integration looking half good, missing the css files for some reason and all assemblies end up beside the exe than inside vl folder. But maybe that should be ignored for now, makes packaging much easier and in the end it's not really our issue where the assemblies end up - could also be solved by some custom move assembly task put into the consuming game project directly.

---
+++ src/VL.Xenko.Integration/GameExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using VL.Core;
+using Xenko.Engine;
+
+namespace VL.Xenko
+{
+    public static class GameExtensions
+    {
+        /// <summary>
+        /// Attaches VL to the game.
+        /// </summary>
+        /// <param name="game">The game to attach VL to.</param>
+        /// <param name="document">The main document VL should open. Defaults to EXE-PATH/vl/Main.vl</param>
+        /// <param name="openEditor">Whether or not to open the VL editor.</param>
+        /// <param name="openEditorInOtherThread">Whether or not to start VL editor in separate thread.</param>
+        /// <param name="goFullscreen">Whether or not the game window should go fullscreen.</param>
+        public static void AttachVL(this Game game, string document = null, bool openEditor = true, bool openEditorInOtherThread = false, bool goFullscreen = false)
+        {
+            // Use lazy initialization so VL will start after the Xenko game is up an running
+            var context = VLContext.Create(
+                document: document ?? Path.Combine(Application.StartupPath, "vl", "Main.vl"),
+                openEditor: openEditor,
+                openEditorInOtherThread: openEditorInOtherThread,
+                useInternalTimer: false,
+                lazy: true);
+
+            // Register the context and the session in Xenko so we can fetch it later
+            game.Services.AddService(context);
+            game.Services.AddService(context.Session);
+            game.Services.AddService(context.Runtime);
+
+            // Register the Xenko game in our registry so node factories can fetch it later
+            ServiceRegistry.Default.RegisterService(game);
+
+            // Register the main synchronization context
+            ServiceRegistry.Default.RegisterService(SynchronizationContext.Current);
+
+            // Game is driving the message loop so tell windows forms about it
+            Application.RegisterMessageLoop(() => game.IsRunning);
+
+            Game.GameStarted += (s, e) =>
+            {
+                if (s == game)
+                {
+                    var script = new VLScript(context, game, goFullscreen);
+                    game.Script.Add(script);
+                    game.Window.AllowUserResizing = true;
+                }
+            };
+
+            // Shutdown the game when VL editor shuts down
+            context.ThreadExit += (s, e) => game.Exit();
+            // Shutdown VL when game shuts down
+            game.Exiting += (s, e) => context.Dispose();
+        }
+    }
+}
---
+++ src/VL.Xenko.Integration/VLScript.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using Xenko.Engine;
+using Xenko.Graphics;
+
+namespace VL.Xenko
+{
+    // TODO: VL script should talk to one IVLNode so we can also feed it data properly from Xenko as well as exposing its pins to Xenko editor
+    public class VLScript : SyncScript
+    {
+        // TODO: Get rid of me
+        public static Game GameInstance { get; private set; }
+
+        readonly VLContext FContext;
+        readonly bool FGoFullscreen;
+
+        // Declared public member fields and properties will show in the game studio
+        public VLScript(VLContext context, Game game, bool goFullscreen)
+        {
+            FContext = context;
+            FGoFullscreen = goFullscreen;
+            Game = game;
+            GameInstance = game;
+        }
+
+        public new Game Game { get; }
+
+        public override void Start()
+        {
+            if (FGoFullscreen)
+            {
+                var gfxOutput = GraphicsAdapterFactory.Adapters[0].Outputs;
+                var displayMode = gfxOutput[0].CurrentDisplayMode;
+
+                var screenWidth = Math.Min(displayMode.Width, 1920);
+                var maxHeight = displayMode.AspectRatio < 1.7f ? 1200 : 1080;
+                var screenHeight = Math.Min(displayMode.Height, maxHeight);
+
+                Game.GraphicsDeviceManager.PreferredBackBufferWidth = screenWidth;
+                Game.GraphicsDeviceManager.PreferredBackBufferHeight = screenHeight;
+                Game.GraphicsDeviceManager.IsFullScreen = true;
+                Game.GraphicsDeviceManager.ApplyChanges();
+            }
+        }
+
+        public override void Update()
+        {
+            // Update all VL root nodes
+            FContext.Update();
+        }
+    }
+}
