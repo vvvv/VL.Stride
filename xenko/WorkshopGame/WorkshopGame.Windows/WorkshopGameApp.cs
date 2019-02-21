5aedb20e0e80ea81701099160ef7351524372477
vvvv-dottore <natan.sinigaglia@gmail.com>
2019-02-21 21:08:15 +0100

    Added workshop game with particle GPU example

---
+++ xenko/WorkshopGame/WorkshopGame.Windows/WorkshopGameApp.cs
@@ -0,0 +1,20 @@
+using System;
+using Xenko.Engine;
+using VL.Xenko;
+
+namespace WorkshopGame.Windows
+{
+    class WorkshopGameApp
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                var vlMainDoc = @"..\..\..\vl\Main.vl";
+                game.AttachVL(vlMainDoc);
+                game.Run();
+            }
+        }
+    }
+}
