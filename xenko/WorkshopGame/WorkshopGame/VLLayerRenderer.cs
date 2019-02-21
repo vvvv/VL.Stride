b9fc1cd3665c966a6c5d6f9306d50cf72b5d4193
tebjan <tebjan@vvvv.org>
2019-02-21 22:08:15 +0100

    shorter layer render code

--- xenko/WorkshopGame/WorkshopGame/VLLayerRenderer.cs
+++ xenko/WorkshopGame/WorkshopGame/VLLayerRenderer.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VL.Xenko.Rendering;
-
-namespace WorkshopGame
+﻿namespace VL.Xenko.Rendering
 {
     public class VLLayerRenderer : LayerRenderFeature
     {
