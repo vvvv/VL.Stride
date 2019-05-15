141681d82015c082658eacdd0361fdbfbe04b142
tebjan <tebjan@vvvv.org>
2019-05-15 04:15:24 +0200

    Group nodes now use pin group feature, added first version of stream out instancer

--- packages/VL.Xenko/src/Layer/Groups.cs
+++ packages/VL.Xenko/src/Layer/Groups.cs
@@ -9,52 +9,6 @@ using Xenko.Rendering;
 namespace VL.Xenko.Layer
 {
     /// <summary>
-    /// Groups multiple layers into one. Layers are rendered from left to right.
-    /// </summary>
-    public sealed class LowLevelAPIGroup : ILowLevelAPIRender
-    {
-        ILowLevelAPIRender FUpstreamLayer1;
-        ILowLevelAPIRender FUpstreamLayer2;
-
-        public ILowLevelAPIRender Update(ILowLevelAPIRender input, ILowLevelAPIRender input2)
-        {
-            FUpstreamLayer1 = input;
-            FUpstreamLayer2 = input2;
-            return this;
-        }
-
-        void ILowLevelAPIRender.Initialize()
-        {
-            FUpstreamLayer1?.Initialize();
-            FUpstreamLayer2?.Initialize();
-        }
-
-        void ILowLevelAPIRender.Collect(RenderContext context)
-        {
-            FUpstreamLayer1?.Collect(context);
-            FUpstreamLayer2?.Collect(context);
-        }
-
-        void ILowLevelAPIRender.Extract()
-        {
-            FUpstreamLayer1?.Extract();
-            FUpstreamLayer2?.Extract();
-        }
-
-        void ILowLevelAPIRender.Prepare(RenderDrawContext context)
-        {
-            FUpstreamLayer1?.Prepare(context);
-            FUpstreamLayer2?.Prepare(context);
-        }
-
-        void ILowLevelAPIRender.Draw(RenderContext renderContext, RenderDrawContext drawContext, RenderView renderView, RenderViewStage renderViewStage, CommandList commandList)
-        {
-            FUpstreamLayer1?.Draw(renderContext, drawContext, renderView, renderViewStage, commandList);
-            FUpstreamLayer2?.Draw(renderContext, drawContext, renderView, renderViewStage, commandList);
-        }
-    }
-
-    /// <summary>
     /// Groups a sequence of layers into one.
     /// </summary>
     public sealed class LowLevelAPISpectralGroup : ILowLevelAPIRender
@@ -95,36 +49,6 @@ namespace VL.Xenko.Layer
         {
             foreach (var upstreamLayer in FUpstreamLayers)
                 upstreamLayer?.Draw(renderContext, drawContext, renderView, renderViewStage, commandList);
-        }
-    }
-
-    /// <summary>
-    /// Groups multiple entities into one.
-    /// </summary>
-    public sealed class EntityGroup : IDisposable
-    {
-        readonly EntityLink link1;
-        readonly EntityLink link2;
-        readonly Entity self;
-
-        public EntityGroup()
-        {
-            self = new Entity();
-            link1 = new EntityLink(self);
-            link2 = new EntityLink(self);
-        }
-
-        public Entity Update(Entity input, Entity input2)
-        {
-            link1.Child = input;
-            link2.Child = input2;
-            return self;
-        }
-
-        public void Dispose()
-        {
-            link1.Dispose();
-            link2.Dispose();
         }
     }
 
