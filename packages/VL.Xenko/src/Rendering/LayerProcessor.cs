017136f39f6947d924be6f3f69f4889d5113fb92
tebjan <tebjan@vvvv.org>
2019-09-02 05:06:28 +0200

    WIP: TextureFX

--- packages/VL.Xenko/src/Rendering/LayerProcessor.cs
+++ packages/VL.Xenko/src/Rendering/LayerProcessor.cs
@@ -37,7 +37,7 @@ namespace VL.Xenko.Rendering
                 if (myEntityComponent.Enabled)
                 {
                     //update world matrix befor rendering
-                    myEntityComponent.Layer.SetEntityWorldMatrix(myEntityComponent.Entity.Transform.WorldMatrix);
+                    myEntityComponent.Layer?.SetEntityWorldMatrix(myEntityComponent.Entity.Transform.WorldMatrix);
                     myRenderObject.SingleCallPerFrame = myEntityComponent.SingleCallPerFrame;
                     myRenderObject.Layer = myEntityComponent.Layer;
                 }
