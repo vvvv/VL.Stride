d9a76d310ae1dcc69aa4f53697437f89698d348f
tebjan <tebjan@vvvv.org>
2019-10-31 22:25:27 +0100

    WIP: LoadProject, Prefabs and Scene working

---
+++ packages/VL.Xenko.Windows/src/Assets/IAssetBuilderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Xenko.Windows.Assets
+{
+    /// <summary>
+    /// This is just used as a generic key for a singleton 
+    /// </summary>
+    public interface IAssetBuilderService
+    {
+    }
+}
