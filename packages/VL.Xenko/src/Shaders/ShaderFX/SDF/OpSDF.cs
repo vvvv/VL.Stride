5ec1d82d3e739b6d48b0408d41321b2240fffec4
everyoneishappy <everoneishappy@users.noreply.github.com>
2019-11-14 19:34:54 +0100

    Added UnionSDF and BoxSDF

---
+++ packages/VL.Xenko/src/Shaders/ShaderFX/SDF/OpSDF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xenko.Rendering.Materials;
+using Xenko.Shaders;
+using Xenko.Core.Mathematics;
+using static VL.Xenko.Shaders.ShaderFX.ShaderFXUtils;
+
+namespace VL.Xenko.Shaders.ShaderFX
+{
+    /// <summary>
+    /// Represents any shader that implements SDF3D with input compositions
+    /// </summary>
+    public class OpSDF : Funk2In1Out<float, float, float>
+    {
+        public OpSDF(string functionName, IEnumerable<KeyValuePair<string, IComputeNode>> inputs)
+            : base(functionName, inputs)
+        {
+           
+        }
+    }
+}
