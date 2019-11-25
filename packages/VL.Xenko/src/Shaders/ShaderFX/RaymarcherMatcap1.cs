8b7522d8f615aeae5cb087581ec0408a96901320
everyoneishappy <everoneishappy@users.noreply.github.com>
2019-11-25 18:30:31 +0100

    merge fixes

--- packages/VL.Xenko/src/Shaders/ShaderFX/RaymarcherMatcap1.cs
+++ packages/VL.Xenko/src/Shaders/ShaderFX/RaymarcherMatcap1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VL.Xenko.Shaders.ShaderFX.Functions;
 using Xenko.Core.Mathematics;
 using Xenko.Graphics;
 using Xenko.Rendering;
