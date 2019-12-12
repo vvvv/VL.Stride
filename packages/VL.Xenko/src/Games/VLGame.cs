51ef021664073b79ce191cb52ea64508f678ab2d
tebjan <tebjan@vvvv.org>
2019-12-12 19:07:57 +0100

    updated to local xenko build

---
+++ packages/VL.Xenko/src/Games/VLGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xenko.Engine;
+using Xenko.Games;
+
+namespace VL.Xenko.Games
+{
+    public class VLGame : Game
+    {
+        public VLGame()
+            : base()
+        {
+            //InternalTick = false;
+        }
+    }
+}
