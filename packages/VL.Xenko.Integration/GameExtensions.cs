3bc3fe173d71031176d0fdbf3addef9b6781bf34
tebjan <tebjan@vvvv.org>
2020-01-17 23:56:12 +0100

    moved VLGame related code to it's own project

--- packages/VL.Xenko.Integration/GameExtensions.cs
+++ packages/VL.Xenko.Integration/GameExtensions.cs
@@ -4,57 +4,12 @@ using System.Threading;
 using System.Windows.Forms;
 using VL.Core;
 using VL.Xenko.Assets;
-using VL.Xenko.Games;
 using Xenko.Engine;
-using Xenko.Games;
-using Xenko.Graphics;
-using VL.Xenko.EffectLib;
 
 namespace VL.Xenko
 {
     public static class GameExtensions
     {
-        public static void CreateVLGame(out VLGame output, out Action runCallback)
-        {
-            var game = new VLGame();
-#if DEBUG
-            game.GraphicsDeviceManager.DeviceCreationFlags |= DeviceCreationFlags.Debug;
-#endif
-
-            Game.GameStarted += (s, e) =>
-            {
-                if (s == game)
-                {
-                    var script = new VLScript(null, game, false);
-                    var assetBuildService = new AssetBuildService();
-                    game.Services.AddService(assetBuildService);
-                    game.Script.Add(assetBuildService);
-                    game.Window.AllowUserResizing = true;
-
-                    MultiGameEffectNodeFactory.Instance?.AddGame(game, SynchronizationContext.Current);
-
-                }
-            };
-
-            Game.GameDestroyed += (s, e) =>
-            {
-                if (s == game)
-                {
-                    MultiGameEffectNodeFactory.Instance?.RemoveGame(game);
-                }
-            };
-
-            var context = new GameContextWinforms(null, 0, 0, isUserManagingRun: true);
-            game.Run(context);
-            game.AddLayerRenderFeature();
-            runCallback = context.RunCallback;
-
-            context.Control.Show();
-
-            output = game;
-        }
-
-
         /// <summary>
         /// Attaches VL to the game.
         /// </summary>
---
+++ packages/VL.Xenko.Lib/src/VLScript.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VL.Xenko.Core;
+using Xenko.Engine;
+using Xenko.Graphics;
+
+namespace VL.Xenko
+{
+    // TODO: VL script should talk to one IVLNode so we can also feed it data properly from Xenko as well as exposing its pins to Xenko editor
+    public class VLScript : AsyncScript
+    {
+        /// <summary>
+        /// Recommended default doc while developing.
+        /// </summary>
+        public static string MainVLDocSrc => Path.Combine(Application.StartupPath,"..", "..", "..", "vl", "Main.vl");
+
+        // TODO: Get rid of me
+        public static Game GameInstance { get; private set; }
+
+        private readonly Action VLUpdate;
+        readonly VLContext FContext;
+        readonly bool FGoFullscreen;
+
+        // Declared public member fields and properties will show in the game studio
+        public VLScript(VLContext context, Game game, bool goFullscreen)
+        {
+            FContext = context;
+            FGoFullscreen = goFullscreen;
+            Game = game;
+            GameInstance = game;
+
+            if (FContext != null)   
+                VLUpdate = FContext.Update;
+        }
+
+        public new Game Game { get; }
+
+        public override async Task Execute()
+        {
+            if (FGoFullscreen)
+            {
+                //if (FContext.Runtime is RuntimeHost rth)
+                //    rth.Mode = Lang.Symbols.RunMode.Paused;
+
+                var gfxOutput = GraphicsAdapterFactory.Adapters[0].Outputs;
+                var displayMode = gfxOutput[0].CurrentDisplayMode;
+
+                var screenWidth = Math.Min(displayMode.Width, 1920);
+                var maxHeight = displayMode.AspectRatio < 1.7f ? 1200 : 1080;
+                var screenHeight = Math.Min(displayMode.Height, maxHeight);
+
+                Game.GraphicsDeviceManager.PreferredBackBufferWidth = screenWidth;
+                Game.GraphicsDeviceManager.PreferredBackBufferHeight = screenHeight;
+                Game.GraphicsDeviceManager.IsFullScreen = true;
+                Game.GraphicsDeviceManager.ApplyChanges();
+            }
+
+            while (true)
+            {
+                await Script.NextFrame();
+                // Update all VL root nodes
+                //await Task.Run(VLUpdate);
+                FContext?.Update();
+            }
+        }
+    }
+}
