de4a195bba3575993122be43fe5d4865790bdfbf
tebjan <tebjan@vvvv.org>
2020-03-21 18:04:20 +0100

    fix for SetParent which is now in xenkos code base.

--- packages/VL.Xenko/src/Engine/EntityExtensions.cs
+++ packages/VL.Xenko/src/Engine/EntityExtensions.cs
@@ -2,9 +2,5 @@
 {
     public static class EntityExtensions
     {
-        public static void SetParent(this Entity entity, Entity parent)
-        {
-            entity.Transform.Parent = parent?.Transform;
-        }
     }
 }
