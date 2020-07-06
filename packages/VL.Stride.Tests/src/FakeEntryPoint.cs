52c74dced50aac259361a2c329e5f71f0d18b8f4
tebjan <tebjan@vvvv.org>
2020-07-06 23:11:04 +0200

    Fixed tests project and all tests are green

---
+++ packages/VL.Stride.Tests/src/FakeEntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL.Stride.Tests
+{
+    public static class FakeEntryPoint
+    {
+        // Although we don't need it, this makes the compiler happy
+        public static void Main(string[] args)
+        { }
+    }
+}
