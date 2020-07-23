679ff1f6943718f8e9e01e4fd061a148e76b7020
Elias Holzer <elias@vvvv.org>
2020-07-23 12:22:06 +0200

    Added and integrated RendererScheduler and GameSystemScheduler nodes - will need review on remaining nodes using legacy AutoDraw (now red)

--- packages/VL.Stride.Runtime/src/Games/VLGame.cs
+++ packages/VL.Stride.Runtime/src/Games/VLGame.cs
@@ -1,34 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using VL.Stride.Rendering;
 using Stride.Engine;
-using Stride.Games;
-using Stride.Rendering;
 using Stride.Engine.Design;
-using VL.Stride.Engine;
 
 namespace VL.Stride.Games
 {
     public class VLGame : Game
     {
-        /// <summary>
-        /// The current game instance, each game region sets this field before its inner Create/Update.
-        /// It gets restored to the previouse value (if it was not null) after Update of the region.
-        /// </summary>
-        // TODO: Get rid of me once we have scoped variables
-
-        [ThreadStatic]
-        public static VLGame GameInstance;
-
         internal readonly SchedulerSystem SchedulerSystem;
 
         public VLGame()
             : base()
         {
-            GameInstance = this;
-
             SchedulerSystem = new SchedulerSystem(Services);
             Services.AddService<IGameSystemScheduler>(SchedulerSystem);
             Services.AddService<IRendererScheduler>(SchedulerSystem);
@@ -53,25 +35,6 @@ namespace VL.Stride.Games
             base.Initialize();
 
             GameSystems.Add(SchedulerSystem);
-        }
-
-        public void AddLayerRenderFeature()
-        {
-            var renderStages = SceneSystem.GraphicsCompositor.RenderStages;
-            var opaqueStage = renderStages.FirstOrDefault(s => s.Name == "Opaque") ?? renderStages.FirstOrDefault();
-
-            if (opaqueStage != null && SceneSystem.GraphicsCompositor.RenderFeatures.None(rf => rf is EntityRendererRenderFeature))
-            {
-                var stageSelector = new SimpleGroupToRenderStageSelector()
-                {
-                    RenderStage = opaqueStage
-                };
-
-                var layerRenderer = new EntityRendererRenderFeature();
-
-                layerRenderer.RenderStageSelectors.Add(stageSelector);
-                SceneSystem.GraphicsCompositor.RenderFeatures.Add(layerRenderer);
-            }
         }
     }
 }
