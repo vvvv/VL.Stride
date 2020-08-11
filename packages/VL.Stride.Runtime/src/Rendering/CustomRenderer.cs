030b0da44fbfbefc73a340dd9a9d93f649f24c31
Elias Holzer <elias@vvvv.org>
2020-08-11 17:58:08 +0200

    New node CustomRenderer (replacing CustomSceneRenderer as well as OnDraw) which allows to patch a renderer with a stateful region and removed VLGraphicsCompositor by using a SceneRendererCollection and the CustomRenderer.

---
+++ packages/VL.Stride.Runtime/src/Rendering/CustomRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Stride.Rendering;
+using Stride.Rendering.Compositing;
+using System;
+
+namespace VL.Stride.Rendering
+{
+    /// <summary>
+    /// A renderer which can provide <see cref="RendererBase.Draw"/> implementation with a stateful region.
+    /// </summary>
+    public sealed class CustomRenderer<TState> : SceneRendererBase
+        where TState : class
+    {
+        private Func<TState> CreateFunc;
+        private Func<TState, RenderDrawContext, TState> UpdateFunc;
+        private TState State;
+
+        public void Update(Func<TState> create, Func<TState, RenderDrawContext, TState> update)
+        {
+            CreateFunc = create;
+            UpdateFunc = update;
+        }
+
+        protected override void DrawCore(RenderContext context, RenderDrawContext drawContext)
+        {
+            var state = State ?? CreateFunc?.Invoke();
+            State = UpdateFunc?.Invoke(state, drawContext);
+        }
+
+        protected override void Destroy()
+        {
+            if (State is IDisposable disposable)
+                disposable.Dispose();
+
+            base.Destroy();
+        }
+    }
+}
