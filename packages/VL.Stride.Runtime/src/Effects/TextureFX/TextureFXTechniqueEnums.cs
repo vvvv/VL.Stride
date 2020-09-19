fa5c0c0096887b852e3b388efe42476708fb8808
Maria Heine <altanhir@gmail.com>
2020-09-19 23:35:08 +0200

    Added Mosaic shader and other stuff i forgot to commit oops

--- packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
@@ -7,4 +7,6 @@ using System.Threading.Tasks;
 namespace VL.Stride.Effects.TextureFX
 {
     public enum ColorRampTechnique { RGB, Hue, Luma, Saturation };
+
+    public enum LevelsClampType { None, Top, Bottom, Both };
 }
