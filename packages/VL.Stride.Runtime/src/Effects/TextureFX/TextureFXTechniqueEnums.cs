bc527a5a1e897a6014dac9bf6b67b50227b15498
Maria Heine <altanhir@gmail.com>
2020-10-01 21:15:49 +0200

    Added blend shader, dized stripes shader, added 2d rotation methods to shader utils, added base Masked shader for all shaders using a mask

--- packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
@@ -9,4 +9,6 @@ namespace VL.Stride.Effects.TextureFX
     public enum ColorRampTechnique { RGB, Hue, Luma, Saturation };
     public enum LevelsClampType { None, Top, Bottom, Both };
     public enum TunnelsDistortionTechnique { Square, Cylinder, Fly };
+    public enum GlowTechnique { Pre, Glow, Mix };
+    public enum BlendTechnique { Normal, Add, Subtract, Screen, Multiply, Darken, Lighten, Difference, Exclusion, Overlay, Hardlight, Softlight, Dodge, Burn, Reflect, Glow, Freeze, Heat, Divide }; 
 }
