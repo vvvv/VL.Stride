b4385ea7cf042f83e00e6168653839ce3e77da0c
Elias Holzer <elias@vvvv.org>
2020-11-24 16:46:07 +0100

    [TextureFX] Cleaned up Levels node

--- packages/VL.Stride.Runtime/src/Effects/TextureFX/Filters/ColorManipulation/Levels/LevelsBase.sdsl.cs
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/Filters/ColorManipulation/Levels/LevelsBase.sdsl.cs
@@ -16,7 +16,7 @@ using Buffer = Stride.Graphics.Buffer;
 
 namespace Stride.Rendering
 {
-    public static partial class Levels_TextureFXKeys
+    public static partial class LevelsBaseKeys
     {
         public static readonly ValueParameterKey<Color4> fromBlack = ParameterKeys.NewValue<Color4>(new Color4(0,0,0,0));
         public static readonly ValueParameterKey<Color4> fromWhite = ParameterKeys.NewValue<Color4>(new Color4(0,0,1,1));
