0b873156ff1f164614339455aeda337c4a908f8d
bj-rn <mail@plain-vanilla.net>
2021-01-05 18:14:29 +0100

    Enums now ordered alphabetically in TextureFXTechniqueEnums.cs

--- packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 
 namespace VL.Stride.Effects.TextureFX
 {
-   
+    public enum AlphaChannel { Average, R, G, B, A };
+    public enum BlendTechnique { Normal, Add, Subtract, Screen, Multiply, Darken, Lighten, Difference, Exclusion, Overlay, Hardlight, Softlight, Dodge, Burn, Reflect, Glow, Freeze, Heat, Divide };
     public enum BumpTechnique { Directional, Point };
     public enum ChannelKeyingTechnique { Alpha, Red, Green, Blue, Luma, Saturation };
     public enum ColorConvertTechnique { Alpha, Red, Green, Blue, Hue, Saturation, Value, HSVtoRGB, RGBtoHSV };
     public enum ColorMapTechnique { Hue, HueSaturation, HueValue, Luma, RedBlue, RGBA, SaturationValue, Tone, Value };
     public enum ColorRampTechnique { RGB, Hue, Luma, Saturation };
+    public enum GlowTechnique { Pre, Glow, Mix };
     public enum HaloTechnique { Linear, Smooth, Spike, Textured };
     public enum LevelsClampType { None, Top, Bottom, Both };
-    public enum TunnelsDistortionTechnique { Square, Cylinder, Fly };
-    public enum GlowTechnique { Pre, Glow, Mix };
-    public enum AlphaChannel { Average, R, G, B, A };
     public enum NoiseType { Perlin, PerlinGrad, Value, ValueGrad, Simplex, SimplexGrad, WorleyFast, WorleyFastGrad };
-    public enum BlendTechnique { Normal, Add, Subtract, Screen, Multiply, Darken, Lighten, Difference, Exclusion, Overlay, Hardlight, Softlight, Dodge, Burn, Reflect, Glow, Freeze, Heat, Divide }; 
+    public enum PaletteTechnique { HSL, HSV, Radial };
+    public enum TunnelsDistortionTechnique { Square, Cylinder, Fly };
+
+
+
+
 }
