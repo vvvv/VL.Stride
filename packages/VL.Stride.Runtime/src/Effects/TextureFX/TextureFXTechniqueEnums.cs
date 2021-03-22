2d78bca4a644f0125f3a4719177ea35edc825c02
joreg <joreg@vvvv.org>
2021-03-22 22:15:40 +0100

    Further tweaks for the manual merge of PR #393

--- packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/TextureFXTechniqueEnums.cs
@@ -8,14 +8,17 @@ namespace VL.Stride.Effects.TextureFX
 {
     public enum AlphaChannel { Average, R, G, B, A };
     public enum BlendTechnique { Normal, Add, Subtract, Screen, Multiply, Darken, Lighten, Difference, Exclusion, Overlay, Hardlight, Softlight, Dodge, Burn, Reflect, Glow, Freeze, Heat, Divide };
+    public enum BumpTechnique { Directional, Point };
+    public enum ChannelKeyingTechnique { Alpha, Red, Green, Blue, Luma, Saturation };
+    public enum ColorConvertTechnique { Alpha, Red, Green, Blue, Hue, Saturation, Value, HSVtoRGB, RGBtoHSV };
+    public enum ColorMapTechnique { Hue, HueSaturation, HueValue, Luma, RedBlue, RGBA, SaturationValue, Tone, Value };
     public enum ColorRampTechnique { RGB, Hue, Luma, Saturation };
     public enum GlowTechnique { Pre, Glow, Mix };
+    public enum HaloTechnique { Linear, Smooth, Spike, Textured };
     public enum LevelsClampType { None, Top, Bottom, Both };
     public enum NoiseType { Perlin, PerlinGrad, Value, ValueGrad, Simplex, SimplexGrad, WorleyFast, WorleyFastGrad };
+    public enum PaletteTechnique { HSL, HSV, Radial };
+    public enum PolarCoordinatesTechnique { Cartesian, Polar };
     public enum SwizzleTechnique { Red, Green, Blue, Alpha };
     public enum TunnelsDistortionTechnique { Square, Cylinder, Fly };
-
-   
-
-    
 }
