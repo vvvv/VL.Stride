890ecc02b22171c83bf7031376d6fe30046e0734
tebjan <tebjan@vvvv.org>
2021-07-27 18:34:24 +0200

    Blur fixes. #444

--- packages/VL.Stride.Runtime/src/Effects/TextureFX/Filters/Blur/Blur_TextureFX.sdsl.cs
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/Filters/Blur/Blur_TextureFX.sdsl.cs
@@ -6,18 +6,4 @@
 // and re-save the associated .sdfx.
 // </auto-generated>
 
-using System;
-using Stride.Core;
-using Stride.Rendering;
-using Stride.Graphics;
-using Stride.Shaders;
-using Stride.Core.Mathematics;
-using Buffer = Stride.Graphics.Buffer;
-
-namespace Stride.Rendering
-{
-    public static partial class Blur_TextureFXKeys
-    {
-        public static readonly ValueParameterKey<float> Strength = ParameterKeys.NewValue<float>();
-    }
-}
+// Nothing to generate
