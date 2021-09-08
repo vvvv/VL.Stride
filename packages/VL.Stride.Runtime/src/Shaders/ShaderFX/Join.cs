5fff937f1d5f27b75c32f36b9db941512f4d0b0b
tebjan <tebjan@vvvv.org>
2021-09-08 15:23:28 +0200

    Adds float3 semantic, float4 Join with float3 and NORMAL node.

--- packages/VL.Stride.Runtime/src/Shaders/ShaderFX/Join.cs
+++ packages/VL.Stride.Runtime/src/Shaders/ShaderFX/Join.cs
@@ -52,4 +52,35 @@ namespace VL.Stride.Shaders.ShaderFX
             return mixin;
         }
     }
+
+    public class JoinVector4Vector3 : ComputeValue<Vector4>
+    {
+        public JoinVector4Vector3(IComputeValue<Vector3> v, IComputeValue<float> w)
+        {
+            V = v;
+            W = w;
+
+            ShaderName = "JoinFloat4Float3";
+        }
+
+        public IComputeValue<Vector3> V { get; }
+        public IComputeValue<float> W { get; }
+
+        public override IEnumerable<IComputeNode> GetChildren(object context = null)
+        {
+            return ReturnIfNotNull(V, W);
+        }
+
+        public override ShaderSource GenerateShaderSource(ShaderGeneratorContext context, MaterialComputeColorKeys baseKeys)
+        {
+            var shaderSource = new ShaderClassSource(ShaderName);
+
+            var mixin = shaderSource.CreateMixin();
+
+            mixin.AddComposition(V, "v", context, baseKeys);
+            mixin.AddComposition(W, "w", context, baseKeys);
+
+            return mixin;
+        }
+    }
 }
