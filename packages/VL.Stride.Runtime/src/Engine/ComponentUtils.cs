5706a6ef667b0d38dd41c6e8e7d75e0615f487e9
Elias Holzer <elias@vvvv.org>
2022-02-18 19:25:48 +0100

    Retrieve the parent manager of a component through an internal ConditionalWeakTable instead of S/R mechanism
    This way we can be sure that all components get wrapped by a parent manager and will be attached to the entity itself.
    In the previous approach this only worked for components wrapped by us - if one was using other components and didn't setup the required parent manager around it the component was not getting picked up by the Entity node.

--- packages/VL.Stride.Runtime/src/Engine/ComponentUtils.cs
+++ packages/VL.Stride.Runtime/src/Engine/ComponentUtils.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Disposables;
+using System.Runtime.CompilerServices;
 using VL.Core;
 using VL.Lib.Collections.TreePatching;
-using VL.Lib.Experimental;
 
 namespace VL.Stride.Engine
 {
     public static class ComponentUtils
     {
+        private static readonly ConditionalWeakTable<EntityComponent, TreeNodeParentManager<Entity, EntityComponent>> managers = new ConditionalWeakTable<EntityComponent, TreeNodeParentManager<Entity, EntityComponent>>();
+
         /// <summary>
         /// Installs a <see cref="TreeNodeParentManager{TParent, TNode}"/> for the given component.
         /// The manager will be used by the Entity node to ensure that the Component is attached to one entity only.
@@ -19,13 +21,10 @@ namespace VL.Stride.Engine
         /// <param name="nodeContext">The path of the node will be used to generate the warning.</param>
         /// <param name="container">The container to which the lifetime of the generated objects will be tied to.</param>
         /// <returns>The component itself.</returns>
-        public static TComponent WithParentManager<TComponent>(this TComponent component, NodeContext nodeContext, CompositeDisposable container)
+        public static TComponent WithParentManager<TComponent>(this TComponent component, NodeContext nodeContext, ICollection<IDisposable> container)
             where TComponent : EntityComponent
         {
-            var manager = new TreeNodeParentManager<Entity, EntityComponent>(component, (e, c) => e.Add(c), (e, c) => e.Remove(c))
-                .DisposeBy(container);
-
-            var sender = new Sender<object, object>(nodeContext, component, manager)
+            var manager = GetParentManager(component)
                 .DisposeBy(container);
 
             // Subscribe to warnings of the manager and make them visible in the patch
@@ -47,6 +46,16 @@ namespace VL.Stride.Engine
                 foreach (var m in messages)
                     VL.Lang.PublicAPI.Session.ToggleMessage(m, on);
             }
+        }
+
+        /// <summary>
+        /// Retrieves the parent manager for a component. A manager will be created on the fly and registered internally for the component should it not have been created yet.
+        /// </summary>
+        /// <param name="component">The component for which to retrieve a manager for.</param>
+        /// <returns>The parent manager for the component.</returns>
+        public static TreeNodeParentManager<Entity, EntityComponent> GetParentManager(this EntityComponent component)
+        {
+            return managers.GetValue(component, component => new TreeNodeParentManager<Entity, EntityComponent>(component, (e, c) => e.Add(c), (e, c) => e.Remove(c)));
         }
     }
 }
