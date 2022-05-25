2085fb3023b77e056ac21520888507915499f673
ravazquez <ravazquez@gmail.com>
2022-05-24 21:19:35 -0600

    Rename GridBoxModel to BoxMesh2 and review properties

--- packages/VL.Stride.Runtime/src/Rendering/Models/BoxMesh2.cs
+++ packages/VL.Stride.Runtime/src/Rendering/Models/BoxMesh2.cs
@@ -9,38 +9,26 @@ namespace VL.Stride.Rendering.Models
     /// <summary>
     /// Class used to generate a Stride Grid box model mesh using geometry3Sharp
     /// </summary>
-    [DataContract("GridBoxModel")]
-    [Display("GridBoxModel")] // This name shows up in the procedural model dropdown list
-    public class GridBoxModel : PrimitiveProceduralModelBase
+    [DataContract("BoxMesh2")]
+    [Display("BoxMesh2")] // This name shows up in the procedural model dropdown list
+    public class BoxMesh2 : PrimitiveProceduralModelBase
     {
-        /// <summary>
-        /// GridBox's center
-        /// </summary>
-        [DataMember(10)]
-        public Vector3 Center { get; set; } = Vector3.Zero;
-
         /// <summary>
         /// GridBox's amount of vertices per edge
         /// </summary>
-        [DataMember(11)]
-        public int EdgeVertices { get; set; } = 2;
-
-        /// <summary>
-        /// GridBox's size or extent
-        /// </summary>
-        [DataMember(12)]
-        public Vector3 Extent { get; set; } = Vector3.One;
+        [DataMember(10)]
+        public int Tessellation { get; set; } = 2;
 
         /// <summary>
         /// 
         /// </summary>
-        [DataMember(13)]
+        [DataMember(11)]
         public bool Clockwise { get; set; } = false;
 
         /// <summary>
         /// 
         /// </summary>
-        [DataMember(14)]
+        [DataMember(12)]
         public bool SharedVertices { get; set; } = false;
 
         /// <summary>
@@ -51,26 +39,14 @@ namespace VL.Stride.Rendering.Models
         {
             var generator = new GridBox3Generator
             {
-                Box = ToBox3d(Center, Extent),
-                EdgeVertices = EdgeVertices,
+                EdgeVertices = Tessellation,
                 Clockwise = Clockwise,
                 NoSharedVertices = !SharedVertices,
             };
 
             var meshGenerator = generator.Generate();
 
-            return Utils.ToGeometricMeshData(meshGenerator.Generate().MakeDMesh(), "GridBoxModel");
-        }
-
-        /// <summary>
-        /// Generates a g3 Box3d based on Stride Vector3's for the GridBoxModel's center and extent
-        /// </summary>
-        /// <param name="center">GridBoxModel's center</param>
-        /// <param name="extent">GridBoxModel's extent</param>
-        /// <returns></returns>
-        private Box3d ToBox3d(Vector3 center, Vector3 extent)
-        {
-            return new Box3d(new Vector3d(center.X, center.Y, center.Z), new Vector3d(extent.X, extent.Y, extent.Z));
+            return Utils.ToGeometricMeshData(meshGenerator.Generate().MakeDMesh(), "BoxMesh2");
         }
     }
 }
