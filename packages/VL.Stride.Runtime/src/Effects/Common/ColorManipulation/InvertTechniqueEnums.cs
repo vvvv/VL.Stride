d60868b271f8d03ae4bcb8bb2f32b601ee63d7c7
cloneproduction <clone@cloneproduction.net>
2022-05-27 00:08:54 +0800

    Update Invert

---
+++ packages/VL.Stride.Runtime/src/Effects/Common/ColorManipulation/InvertTechniqueEnums.cs
@@ -0,0 +1,21 @@
+﻿namespace VL.Stride.Effects.Common
+{
+    /// <summary>
+    /// Operands of the Invert nodes.
+    /// </summary>
+    public enum Invert
+    { 
+        RGB,
+        Red,
+        Green,
+        Blue,
+        RedGreen,
+        RedBlue,
+        GreenBlue,
+        Value,
+        Lightness,
+        Saturation,
+        Hue,
+        RGBShift,
+    };
+}
