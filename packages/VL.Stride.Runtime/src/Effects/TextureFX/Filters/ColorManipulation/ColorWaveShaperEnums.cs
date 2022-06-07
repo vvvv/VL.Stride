273fc7dc757cfe4a60452f2150e8db154ce59b48
cloneproduction <clone@cloneproduction.net>
2022-06-07 23:00:39 +0800

    Added Twirl and ColorWaveShaper

---
+++ packages/VL.Stride.Runtime/src/Effects/TextureFX/Filters/ColorManipulation/ColorWaveShaperEnums.cs
@@ -0,0 +1,15 @@
+﻿namespace VL.Stride.Effects.Common
+{
+    /// <summary>
+    /// Operands of the ColorWaveShaper node.
+    /// </summary>
+    public enum ColorWave
+    { 
+        Linear,
+        Inverse,
+        Triangle,
+        Sin,
+        Rectangle,
+        Frac
+    };
+}
